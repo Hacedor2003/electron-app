c6aac40954bcd5b6b5e52ea62e3471d2d8cb6c15
Hacedor2003 <bryanlenier@gmail.com>
2024-09-02 10:17:05 -0400

    Configuracion inicial updater

--- src/main/index.ts
+++ src/main/index.ts
@@ -2,10 +2,17 @@ import { app, shell, BrowserWindow, ipcMain } from 'electron'
 import { join } from 'path'
 import { electronApp, optimizer, is } from '@electron-toolkit/utils'
 import icon from '../../resources/icon.png?asset'
+import { autoUpdater } from 'electron-updater'
+
+let mainWindow
+
+//Basic flags
+autoUpdater.autoDownload = false
+autoUpdater.autoInstallOnAppQuit = true
 
 function createWindow(): void {
   // Create the browser window.
-  const mainWindow = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     width: 900,
     height: 670,
     show: false,
@@ -49,6 +56,10 @@ app.whenReady().then(() => {
     optimizer.watchWindowShortcuts(window)
   })
 
+  autoUpdater.checkForUpdates()
+  /* mainWindow.showMessage(`Checking for updates. Current version ${app.getVersion()}`) */
+  console.log(`Checking for updates. Current version ${app.getVersion()}`)
+
   // IPC test
   ipcMain.on('ping', () => console.log('pong'))
 
@@ -59,6 +70,31 @@ app.whenReady().then(() => {
     // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
+})
+
+/*New Update Available*/
+autoUpdater.on('update-available', () => {
+  /* mainWindow.showMessage(`Update available. Current version ${app.getVersion()}`) */
+  console.log(`Update available. Current version ${app.getVersion()}`)
+  const pth = autoUpdater.downloadUpdate()
+  /* mainWindow.showMessage(pth) */
+  console.log(pth)
+})
+
+autoUpdater.on('update-not-available', () => {
+  /* mainWindow.showMessage(`No update available. Current version ${app.getVersion()}`) */
+  console.log(`No update available. Current version ${app.getVersion()}`)
+})
+
+/*Download Completion Message*/
+autoUpdater.on('update-downloaded', () => {
+  /* mainWindow.showMessage(`Update downloaded. Current version ${app.getVersion()}`) */
+  console.log(`Update downloaded. Current version ${app.getVersion()}`)
+})
+
+autoUpdater.on('error', (info) => {
+  /* mainWindow.showMessage(info) */
+  console.log(info)
 })
 
 // Quit when all windows are closed, except on macOS. There, it's common
